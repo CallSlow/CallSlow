ed6364cff1c7717a87d30d0c260eef0cdf0053b9
Virgile@Z420-NeF.nef <Virgile@Z420-NeF.nef>
2023-05-03 19:12:12 +0200

    Possibilité d'ajouter des contacts depuis une popup + affichage dans la liste.

--- app/src/main/java/com/example/callslow/ui/contact/ContactFragment.java
+++ app/src/main/java/com/example/callslow/ui/contact/ContactFragment.java
@@ -1,15 +1,24 @@
 package com.example.callslow.ui.contact;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -21,12 +30,19 @@ import com.example.callslow.objects.Contacts;
 
 import java.util.ArrayList;
 
-public class ContactFragment extends Fragment implements SearchView.OnQueryTextListener{
+public class ContactFragment extends Fragment implements SearchView.OnQueryTextListener , View.OnClickListener {
 
     private ListView mListView;
     private SearchView mSearchView;
     private ContactAdapter mAdapter;
     private ArrayList<Contact> mContactList;
+
+    private Button addContactBtn, createContactBtn;
+    private EditText nameInput, macInput;
+
+    private PopupWindow popupWindow;
+
+    private View view;
 
 
     private FragmentContactBinding binding;
@@ -52,8 +68,19 @@ public class ContactFragment extends Fragment implements SearchView.OnQueryTextL
         mAdapter = new ContactAdapter(getActivity(), mContactList);
         mListView.setAdapter(mAdapter);
 
+        addContactBtn = (Button) root.findViewById(R.id.addBtn);
+        addContactBtn.setOnClickListener(this);
+
+
+        view = root;
+
 
         return root;
+    }
+
+    private void refresh() {
+        mAdapter = new ContactAdapter(getActivity(), mContactList);
+        mListView.setAdapter(mAdapter);
     }
 
     @Override
@@ -72,5 +99,81 @@ public class ContactFragment extends Fragment implements SearchView.OnQueryTextL
     public boolean onQueryTextChange(String s) {
         mAdapter.getFilter().filter(s);
         return false;
+    }
+
+    private void alertBox(String title, String content) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle(title)
+                .setMessage(content)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do things
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void createContact() {
+        Contact newContact = new Contact(nameInput.getText().toString(),macInput.getText().toString());
+        try {
+            if (!Contacts.getInstance().addContact(newContact)) {
+                alertBox("Contact Existant", "Le contact que vous essayer d'entrer existe déjà !");
+                return;
+            }
+            popupWindow.dismiss();
+            refresh();
+        } catch (Exception e) {
+            alertBox("Une erreur est survenue", e.getMessage());
+        }
+        //
+    }
+
+    private void createPopup() {
+        // Créer une vue qui contient le formulaire
+        View popupView = LayoutInflater.from(requireContext()).inflate(R.layout.contacts_popup, null);
+
+        // Créer la PopupWindow
+        popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //Fond de la popup blanc
+        int color = Color.parseColor("#FFFFFFFF");
+        ColorDrawable background = new ColorDrawable(color);
+
+        createContactBtn = (Button) popupView.findViewById(R.id.button_submitCt);
+        createContactBtn.setOnClickListener(this);
+
+        nameInput = (EditText) popupView.findViewById(R.id.editText_ContactName);
+        macInput = (EditText) popupView.findViewById(R.id.editText_mac);
+
+        //niveau d'opacité (255 = totalement opaque)
+        background.setAlpha(240);
+        popupWindow.setBackgroundDrawable(background);
+        popupWindow.setFocusable(true);
+    }
+
+    public void showPopup() {
+        // Créer la PopupWindow si elle n'existe pas encore
+        if (popupWindow == null) {
+            createPopup();
+        }
+        // Afficher la PopupWindow
+        popupWindow.showAtLocation(mSearchView, Gravity.CENTER, 0, 0);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.addBtn:
+                showPopup();
+                break;
+            case R.id.button_submitCt:
+                createContact();
+                break;
+
+            default:
+                break;
+        }
     }
 }
