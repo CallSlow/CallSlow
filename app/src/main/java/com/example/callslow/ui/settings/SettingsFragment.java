1b4b5d4d0373187233402909219ca855286ca310
mathieu_m <mmaes@adexos.fr>
2023-06-13 17:41:14 +0200

    Edit mac address and username

--- app/src/main/java/com/example/callslow/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/callslow/ui/settings/SettingsFragment.java
@@ -1,25 +1,26 @@
 package com.example.callslow.ui.settings;
 
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.callslow.R;
 import com.example.callslow.databinding.FragmentSettingsBinding;
-import com.example.callslow.objects.Contacts;
 import com.example.callslow.objects.Settings;
 
 import java.util.ArrayList;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     private FragmentSettingsBinding binding;
     private ArrayList<String> settingslist;
@@ -28,41 +29,41 @@ public class SettingsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         SettingsViewModel settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
-
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Settings settings = Settings.getInstance();
 
-        Settings.getInstance().init(getContext());
-        settingslist = Settings.getInstance().getSettings();
+        settings.init(getContext());
+        settingslist = settings.getSettings();
 
-        final TextView textView = binding.textView;
-        EditText macAdress = binding.myMacAdress;
+        EditText editMacAdress = binding.myMacAddress;
         if (settingslist.get(0) != null) {
-            macAdress.setText(settingslist.get(0));
-        } else {
-            WifiManager manager = (WifiManager) getActivity().getApplicationContext().getSystemService(getContext().WIFI_SERVICE);
-            WifiInfo info = manager.getConnectionInfo();
-            String address = info.getMacAddress();
-            macAdress.setText(address);
-            try {
-                Settings.getInstance().changeMacAdress(address);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            editMacAdress.setText(settingslist.get(0));
         }
-        EditText pseudo = binding.myPseudo;
+        EditText editPseudo = binding.myPseudo;
         if (settingslist.get(1) != null) {
-            pseudo.setText(settingslist.get(1));
-        } else {
-            pseudo.setText("Entrez votre pseudo");
-            try {
-                Settings.getInstance().changePseudo(pseudo.getText().toString());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            editPseudo.setText(settingslist.get(1));
         }
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button saveSettingsButton = root.findViewById(R.id.saveSettingsButton);
+        saveSettingsButton.setOnClickListener(this);
+
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText editPseudo = binding.myPseudo;
+        EditText editMacAddress = binding.myMacAddress;
+        try {
+            Settings.getInstance().changePseudo(editPseudo.getText().toString());
+            Settings.getInstance().changeMacAdress(editMacAddress.getText().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     @Override
